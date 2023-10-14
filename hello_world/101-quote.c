e68878b8434bcefe7c6edae1673ff703cc24f08b
jordiindahood* <hazem.ouni123@gmail.com>
2023-10-14 10:43:47 -0700

    advanced task

---
+++ hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+/**
+ *main - print a sentence
+ *Void
+ *DescriptionDescription: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: one
+ */
+int main(void) 
+{ 
+	const char msg[]="\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+	syscall(SYS_write, 1, msg, sizeof(msg)); 
+	return (1); 
+}
