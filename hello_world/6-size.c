eb22bbecfca8eef6c332b3e95c5d02100cc525e5
jordiindahood* <hazem.ouni123@gmail.com>
2023-10-14 07:11:10 -0700

    script 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - print a sentence
+ *Void
+ *DescriptionDescription: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: zero
+ */
+int main(void)
+{
+	printf("%d\n",sizeof(char);
+	printf("%d\n",sizeof(int);
+	printf("%d\n",sizeof(long int);
+	printf("%d\n",sizeof(long long int);
+	printf("%d\n",sizeof(float);
+return (0);
+}
