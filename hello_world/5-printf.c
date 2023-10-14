558ee7b3a865f2d9906e796a7e01b289a6b8dff3
jordiindahood* <hazem.ouni123@gmail.com>
2023-10-14 07:04:04 -0700

    script 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+/**
+ *main - print a sentence
+ *Void
+ *DescriptionDescription: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: zero
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
