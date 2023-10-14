bd9a56757cd0715c8066e2f6f9bd335e4e2060f7
jordiindahood* <hazem.ouni123@gmail.com>
2023-10-14 06:57:34 -0700

    script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+ *main - print a sentence
+ *Void
+ *DescriptionDescription: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
