360cf7b173f45f09bd4fd655a3f08b2bd78a353a
jordiindahood* <hazem.ouni123@gmail.com>
2023-10-14 11:00:19 -0700

    advanced task

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -10,9 +10,9 @@
  * describe your function
  * Return: one
  */
-int main(void) 
+int main(void)
 {
-	const char msg[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-	syscall(SYS_write, 1, msg, sizeof(msg)); 
-	return (1); 
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	syscall(SYS_write, 1, msg, sizeof(msg));
+	return (1);
 }
