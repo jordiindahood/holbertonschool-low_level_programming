28a9bbc785245f4c84cd5a2346b6b76e34f011a1
jordiindahood* <hazem.ouni123@gmail.com>
2023-10-14 11:07:44 -0700

    advanced task

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -12,8 +12,8 @@
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	syscall(SYS_write, 0, msg, sizeof(msg));
 	return (1);
 }
