f4cdd3f0815702c1d33b54e0d03e8431d14014b6
jordiindahood* <hazem.ouni123@gmail.com>
2023-10-14 06:33:14 -0700

    script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void) /*prints exactly "Programming is like building a multilingual puz		zle*/
+int main(void)/*prints exactly 
+		"Programming is like building a multilingual puzzle*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 return 0;
