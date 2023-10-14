33654543d90cd8c215182a2e2f20d08f8bdb90e1
jordiindahood* <hazem.ouni123@gmail.com>
2023-10-14 07:12:48 -0700

    script 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,10 +10,10 @@
  */
 int main(void)
 {
-	printf("%d\n",sizeof(char);
-	printf("%d\n",sizeof(int);
-	printf("%d\n",sizeof(long int);
-	printf("%d\n",sizeof(long long int);
-	printf("%d\n",sizeof(float);
+	printf("%d\n",sizeof(char));
+	printf("%d\n",sizeof(int));
+	printf("%d\n",sizeof(long int));
+	printf("%d\n",sizeof(long long int));
+	printf("%d\n",sizeof(float));
 return (0);
 }
