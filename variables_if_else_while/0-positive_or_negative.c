da81bed808c241739e1eb475f296eca07be9da2b
jordiindahood <hazem.ouni123@gmail.com>
2023-10-16 12:06:45 +0100

    script 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -21,6 +21,7 @@ int main(void)
         printf("%d is negative\n", n);
     else if (n > 0)
         printf("%d is positive\n", n);
-    else printf("%d is zero\n", n);
+    else
+    printf("%d is zero\n", n);
 return (0);
 }
