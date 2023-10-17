1da223fde66dd880ac431c9dc63a5d79ee38a830
jordiindahood <hazem.ouni123@gmail.com>
2023-10-17 12:05:09 +0100

    script 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*more headers goes there*/
+
+/**
+ *main - print a sentence
+ *Void
+ *Description: print alphabet in lowercase
+ * Return: zero
+ */
+int main(void)
+{
+char n;
+char ch ;
+char newline = '\n';
+for (n = '0';   n <= '9';  n++)
+{
+putchar(n);
+}
+for (ch = 'a';  ch <= 'f'; ch++){
+putchar(ch);
+}
+putchar(newline);
+return (0);
+}
