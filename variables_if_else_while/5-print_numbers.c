63cf54a0069ca4449377693f7b14b22993f9e4db
jordiindahood <hazem.ouni123@gmail.com>
2023-10-17 09:24:11 +0100

    script 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*more headers goes there*/
+
+/**
+ *main - print numbers
+ *Void
+ *Description: print numbers form 0 to 9
+ * Return: zero
+ */
+int main(void)
+{
+int n = 0;
+char newline = '\n';
+for (n = 0;   n < 10;    n++)
+{
+printf("%d",n);
+}
+putchar(newline);
+return (0);
+}
