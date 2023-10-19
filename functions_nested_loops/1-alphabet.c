e80420d5c5cba6111aad375525fd1fb68b7b071a
jordiindahood <hazem.ouni123@gmail.com>
2023-10-19 12:51:52 +0100

    script 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+int print_alphabet(void)
 {
-print_alphabet();
+    char ch = 'a';
+char newline = '\n';
+for (ch = 'a';   ch <= 'z';  ch++)
+{
+_putchar(ch);
+}
+_putchar(newline);
 return (0);
 }
