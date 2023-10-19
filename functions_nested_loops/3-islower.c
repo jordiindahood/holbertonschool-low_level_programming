eda304432d8e7d7ec03b75e4669100d64c1e52eb
jordiindahood <hazem.ouni123@gmail.com>
2023-10-19 14:46:52 +0100

    script 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - check the code
+ * Description: check for lowercase character
+ * Return: 1 if c is lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+    return (1);
+    else
+    return (0);
+}
