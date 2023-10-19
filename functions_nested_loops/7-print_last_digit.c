2684b782808942eff1d99d8e6dc65c7bb6e0dc25
jordiindahood <hazem.ouni123@gmail.com>
2023-10-19 16:20:49 +0100

    script 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <time.h>
+#include <stdio.h>
+/*more headers goes there*/
+
+/**
+ * print_last_digit - check the code
+ * Description: return the last digit
+ * @n: int
+ * Return: int
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
