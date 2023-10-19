81924deb0ce14332f5a47d459fad987ec428fe4f
jordiindahood <hazem.ouni123@gmail.com>
2023-10-19 12:51:34 +0100

    main

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,20 +1,2 @@
-#include <stdio.h>
-/*more headers goes there*/
-
-/**
- *main - print a sentence
- *Void
- *Description: print alphabet in lowercase
- * Return: zero
- */
-int print_alphabet(void)
-{
-char ch = 'a';
-char newline = '\n';
-for (ch = 'a';   ch <= 'z';  ch++)
-{
-putchar(ch);
-}
-putchar(newline);
-return (0);
-}
+int print_alphabet(void);
+int _putchar(char c);
