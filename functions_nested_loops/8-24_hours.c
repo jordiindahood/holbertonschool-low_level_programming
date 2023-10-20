ea5dc631ab6cb83a9f2febff17cf278b120a4591
jordiindahood <hazem.ouni123@gmail.com>
2023-10-20 08:44:12 +0100

    script 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/*more headers goes there*/
+
+/**
+ * jack_bauer - check the code
+ * Description: print form 00:00 to 23:59
+ * Return: void
+ */
+ void jack_bauer(void)
+{
+	int i, x, y, z;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (x = 0; x < 9; x++)
+		{
+			if (i == 2 && x >= 3)
+				x = 3;
+			for (y = 0; y < 6; y++)
+			{
+				for (z = 0; z < 10; z++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + x);
+					_putchar(':');
+					_putchar('0' + y);
+					_putchar('0' + z);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
