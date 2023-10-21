9910ad7e3c74dce46cd4b71a6395a090443ddbd1
jordiindahood <hazem.ouni123@gmail.com>
2023-10-21 16:09:47 +0100

    script 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * printnumbers - check the code
+ * Description: print from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i<10;i++)
+		_putchar('0'+i);
+	_putchar('\n');
+}
