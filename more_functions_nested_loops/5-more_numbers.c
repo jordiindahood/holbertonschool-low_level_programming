8fd2e48dfcaae27226fd0cc2025d0ce7ba8b75b8
jordiindahood <hazem.ouni123@gmail.com>
2023-10-22 12:36:21 +0100

    script 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - check the code
+ * Description: print from 0 to 14 10 times
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i != 2 && i != 4)
+				_putchar('0' + i);
+		}
+		_putchar('\n');
+	}
+}
