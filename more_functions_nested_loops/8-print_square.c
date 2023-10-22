c5e6b05c7c3456e15f73d3e0f3fa45be460f03e7
jordiindahood <hazem.ouni123@gmail.com>
2023-10-22 14:47:42 +0100

    script 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - check the code
+ * Description: draw a square
+ * @size : side length of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+            _putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
