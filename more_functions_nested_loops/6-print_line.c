ef59c139aa3a254961f4ea8d9ce4ec8b6eab91ad
jordiindahood <hazem.ouni123@gmail.com>
2023-10-22 13:39:28 +0100

    script 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - check the code
+ * Description: draw a straight line
+ * @n : int =(length of line)
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n < 1)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+	}
+}
