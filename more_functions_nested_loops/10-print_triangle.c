838f2aee986b3e5fd84c30999c15e117a775744b
jordiindahood <hazem.ouni123@gmail.com>
2023-10-22 18:14:08 +0100

    script 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - check the code
+ * Description: draw a triangle
+ * @size : size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int rows = size;
+	int i, j, k;
+	for (i = 0; i < rows; i++)
+	{
+
+		for (j = 0; j < 2 * (rows - i) - 1; j++)
+		{
+			printf(" ");
+		}
+
+		for (k = 0; k <= i; k++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+}
