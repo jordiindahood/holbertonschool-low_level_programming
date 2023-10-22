e5c197fa8052453eda826515baf42dd1788e67ab
jordiindahood <hazem.ouni123@gmail.com>
2023-10-22 14:23:25 +0100

    script 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - check the code
+ * Description: draw a diagonal
+ * @n : int =(length of diagonal)
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; i > j; j++)
+				_putchar(32);
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
