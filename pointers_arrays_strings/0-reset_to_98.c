d129cd7dc7301f3aa4ca951d296e5f9690ab1582
jordiindahood <hazem.ouni123@gmail.com>
2023-10-23 11:02:17 +0100

    script 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - check the code
+ * Description: update the value of a variable to 98
+ * @n : pointer to the number to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
