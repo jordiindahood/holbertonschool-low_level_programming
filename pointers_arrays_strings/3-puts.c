bfe216bbc695d5e8d1669e2dd832492fb58ea9b2
jordiindahood <hazem.ouni123@gmail.com>
2023-10-23 11:54:14 +0100

    script 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - check the code
+ * Description: print a string, followed by a new line
+ * @str : pointer to string
+ * Return: void
+ */
+int _puts(char *str)
+{
+	puts(*str);
+	_putchar('\n');
+}
