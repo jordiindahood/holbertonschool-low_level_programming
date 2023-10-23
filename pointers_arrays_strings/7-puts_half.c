3c3f99bd76abe1fecf8927649d84c50aaa68ede2
jordiindahood <hazem.ouni123@gmail.com>
2023-10-23 17:10:30 +0100

    script 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - check the code
+ * Description: print half the string
+ * @str: pointer to the string to be executed
+ * Return: void
+ */
+void puts2(char *s)
+{
+	int i, n, m;
+
+	n = strlen(s);
+	if (n % 2 == 0)
+		m = n * 2;
+	else
+		m = (n - 1) / 2;
+	for (i = m; i < n; i++)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
