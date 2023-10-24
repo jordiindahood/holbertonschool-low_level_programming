797c8f9c636124450d46b1190c8785b1a07d3cd4
jordiindahood <hazem.ouni123@gmail.com>
2023-10-24 13:41:50 +0100

    script 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,7 +5,7 @@
  * _strcpy - check the code
  * Description: copy an array to another
  * @dest : pointer to destination array
- * @scr : pointer to be copied
+ * @src : pointer to be copied
  * Return: char
  */
 char *_strcpy(char *dest, char *src)
