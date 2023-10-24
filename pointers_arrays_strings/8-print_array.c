4be0913684ca18c6523a008574414d339658bd52
jordiindahood <hazem.ouni123@gmail.com>
2023-10-24 09:38:02 +0100

    script 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,10 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for ( i =0 ; i <n ; i++){
-		printf("%d",*(a+i));
-		if (i <= n-1)
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i <= n - 1)
 			printf(", ");
 	}
 }
