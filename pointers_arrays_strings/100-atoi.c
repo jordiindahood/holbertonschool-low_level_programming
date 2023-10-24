a7cfe75be76f897af8663260804fb1da1ac36d11
jordiindahood <hazem.ouni123@gmail.com>
2023-10-24 17:21:30 +0100

    script 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _atoi - check the code
+ * Description: convert a string to integer
+ * @s : pointer to string
+ * Return: vaule converted from string
+ */
+int _atoi(char *s)
+{
+	
+	return (atoi(s));
+}
