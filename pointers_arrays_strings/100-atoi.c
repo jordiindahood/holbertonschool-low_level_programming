aeb62e2f9e267fe8c768e0d08a29fef64e845bde
jordiindahood <hazem.ouni123@gmail.com>
2023-10-24 17:33:06 +0100

    script 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,6 +10,19 @@
  */
 int _atoi(char *s)
 {
-	
-	return (atoi(s));
+	int i, len = 0;
+	int res;
+
+	len = strlen(s);
+	while (i < len)
+	{
+		if (*(s + i) >= 48 && *(s + i) <= 57)
+		{
+			if (*(s + i - 1) == '-')
+				res = res * 10 + itoa(*(s + i)) * -1;
+			else
+				res = res * 10 + itoa(*(s + i));
+		}
+	}
+	return (res);
 }
