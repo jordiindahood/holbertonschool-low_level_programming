19c800d52c7dc6585f87c17b0d8bb5432f0e655c
jordiindahood <hazem.ouni123@gmail.com>
2023-10-24 09:37:51 +0100

    script 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - check the code
+ * Description: print n alaments of an array of integer
+ * @n : number of cases in @a
+ * @a : array of integer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for ( i =0 ; i <n ; i++){
+		printf("%d",*(a+i));
+		if (i <= n-1)
+			printf(", ");
+	}
+}
