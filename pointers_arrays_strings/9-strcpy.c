c9eddb8d457435f7c43398c512c76010ca328fbe
jordiindahood <hazem.ouni123@gmail.com>
2023-10-24 16:42:29 +0100

    script 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 /**
  * _strcpy - check the code
  * Description: copy an array to another
@@ -10,13 +11,21 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	/*
 	int n, i = 0;
-
-	while (src[n] != '\0')
+	
+	while (*(src+n) != '\0')
 	{
 		n++;
 	}
+	dest = (char *)malloc(sizeof(char)*n);
 	for (i = 0; i <= n; i++)
-		dest[i] = src[i];
+		*(dest+i) = *(src+i);
+	*/
+	int i=0;
+	while (*(src+i)!='\0') {
+		*(dest+i) = *(src+i);
+		i++;
+	}
 	return (dest);
 }
