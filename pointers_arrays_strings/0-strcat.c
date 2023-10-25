2cd61fa3db7fd53910340dfb5a044c195cd1a784
jordiindahood <hazem.ouni123@gmail.com>
2023-10-25 14:57:16 +0100

    script 01

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - check the code
+ * Description: cancatenate two strings
+ * @src : pointer to string SOURCE
+ * @dest : pointer to string DESTINATION
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+    return(strcat(*dest,*src));
+}
