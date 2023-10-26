2ced58f42e3ec3ad824e707b4de9e650dd5fe8c1
jordiindahood <hazem.ouni123@gmail.com>
2023-10-26 16:17:04 +0100

    script 06

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * cap_string - check the code
+ * Description: capitalize all words
+ * @s: pointer to the string to be executed
+ * Return: void
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		while (*(s + i) != '\0')
+		{
+			if (*(s + i) <= 'z' && *(s + i) >= 'a' && *(s + i - 1) == (2||' ' || 9 || '\n' || ',' || ';' || '.' || '!' || '?' || '"' || '(' || ')' || '{' || '}'))
+				*(s + i) -= 32;
+			i++;
+		}
+	}
+	return (s);
+}
