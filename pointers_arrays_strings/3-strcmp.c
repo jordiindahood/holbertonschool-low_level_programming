043611d18c5d1150b56a55c568fc3f7afa97e669
jordiindahood <hazem.ouni123@gmail.com>
2023-10-26 14:15:40 +0100

    script 03

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strcmp - check the code
+ * Description: compare two strings
+ * @s1 : pointer to string1
+ * @s2 : pointer to string2
+ * Return: int
+ */
+char *_strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1,s2));
+}
