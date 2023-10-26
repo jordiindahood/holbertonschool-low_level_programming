080cabdb8841879b7bcbb552f9b2cd1b3721301d
jordiindahood <hazem.ouni123@gmail.com>
2023-10-26 14:10:25 +0100

    script 02

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	if (*dest)
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+		*(dest + i) = *(src + i);
+	while (i < n)
 	{
-		while (i < n && *(src+i) != '\0')
-		{
-			*(dest + i) = *(src + i);
-			i++;
-		}
+		*(dest + i) = '\0';
+		i++;
 	}
 	return (dest);
 }
