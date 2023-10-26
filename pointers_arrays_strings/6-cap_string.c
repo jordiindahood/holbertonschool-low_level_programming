723bdad69911dcdaac07c38ee56059e562387f05
jordiindahood <hazem.ouni123@gmail.com>
2023-10-26 17:33:51 +0100

    script 06

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,8 +7,7 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0;
-	int cap = 0;
+	int i, cap, check = 0;
 
 	if (*s != '\0')
 	{
@@ -16,9 +15,13 @@ char *cap_string(char *s)
 			*(s + i) -= 32;
 		while (*(s + i) != '\0')
 		{
-			if (cap == 0 && (*(s + i) == ' ' || *(s + i) == '\n' || *(s + i) == '\t'))
-				if (*(s + i + 1) <= 'z' && *(s + i + 1) >= 'a')
+			if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '.')
+				check++;
+			if (cap == 0 && check == 1)
+				if (*(s + i + 1) <= 'z' && *(s + i + 1) >= 'a'){
 					cap++;
+					check--;
+				}
 			if (*(s + i) <= 'z' && *(s + i) >= 'a' && cap == 1)
 			{
 				*(s + i) -= 32;
