c98c678236459b449112f9fd932c7459d8dc8fab
jordiindahood <hazem.ouni123@gmail.com>
2023-10-26 09:53:09 +0100

    script 01

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - check the code
+ * Description: cancatenate two strings
+ * @src : pointer to string SOURCE
+ * @n : number of bytes to be concatinated from @src
+ * @dest : pointer to string DESTINATION
+ * Return: *char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l, i;
+
+	l = 0;
+	while (dest[l])
+		l++;
+	for (i = 0; i < n; i++)
+		dest[l++] = src[i];
+	return (dest);
+}
