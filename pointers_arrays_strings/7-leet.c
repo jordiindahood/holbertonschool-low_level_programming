b4327bea8b162be0eae1b2d91246ef32913816e3
jordiindahood <hazem.ouni123@gmail.com>
2023-10-27 11:27:58 +0100

    script 07

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,22 +7,18 @@
  */
 char *leet(char *s)
 {
-	int i, l = 0;
+	int j, i, l = 0;
+	char T[5] = {'A', 'E', 'O', 'T', 'L'};
+	char V[5] = {'4', '3', '0', '7', '1'};
 
-	while (*(s + l))
-		l++;
+	l = strlen(s);
 	for (i = 0; i < l; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i]='4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i]='3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i]='0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i]='7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i]='1';
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == T[j] || s[i] == T[j] + 32)
+				s[i] = V[j];
+		}
 	}
 	return (s);
 }
