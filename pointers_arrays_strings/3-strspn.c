715092b9fce2f6146c5a56fcc0e9793b539a8b31
jordiindahood <hazem.ouni123@gmail.com>
2023-10-28 18:00:40 +0100

    script 003

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,12 +5,6 @@
  * @s : string to be executed
  * @accept : string containnig the carachters to match
  * Return: unsignet int
- *
- * _strcmp - check the code
- * Description: compare two strings
- * @s1 : pointer to string1
- * @s2 : pointer to string2
- * Return: int
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -20,21 +14,9 @@ unsigned int _strspn(char *s, char *accept)
 	len = strlen(accept);
 	while (*(s + i))
 	{
-		if (_strcmp(s + i + len, accept) >= 0)
+		if (_strcmp(s + i + len, accept) == 0)
 			res = len;
 		i++;
-	}
-	return (res);
-}
-int _strcmp(char *s1, char *s2)
-{
-	int res = 0;
-
-	while (*s1 != '\0' && *s2 != '\0' && res == 0)
-	{
-		res = *s1 - *s2;
-		s1++;
-		s2++;
 	}
 	return (res);
 }
