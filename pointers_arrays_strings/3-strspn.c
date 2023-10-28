25f332af726bd26dc60a88e31481687d34d4a6c8
jordiindahood <hazem.ouni123@gmail.com>
2023-10-28 17:25:05 +0100

    script 003

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 	len = strlen(accept);
 	while (*(s + i))
 	{
-		if (s[i + len] == *accept)
+		if (_strcmp(s[i + len] , *accept)==0)
 			res = len;
 		i++;
 	}
