ba13e882e6bb0f88ef60557501c96314b998eae0
jordiindahood <hazem.ouni123@gmail.com>
2023-10-28 15:17:23 +0100

    script 001

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,16 +3,17 @@
  * _memset - check the code
  * Description: fill memory with a constant byte
  * @s : pointer
- * @src : pointer to be copied
- * @n : int
+ * @b : charachter to be copied
+ * @n : number of bytes
  * Return: char*
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i=0;i<n;i++){
-		s[i]=b;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
 	}
 	return (s);
 }
