5be96a18d86ff38e09d7cc8ed5ba206f5fcd0b73
jordiindahood <hazem.ouni123@gmail.com>
2023-10-29 23:49:40 +0100

    script 006

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_chessboard - check the code
+ * Description: print a chess board
+ * @a : pionter to 2D array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		putchar('\n');
+	}
+}
