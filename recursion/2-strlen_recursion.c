992d0b5bc3e7f10a285d295fcd06f6a2cf2d7576
jordiindahood <hazem.ouni123@gmail.com>
2023-10-30 10:42:38 +0100

    script 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - check the code
+ * Description: count the length of a string
+ * @s : pointer to string
+ * Return: Void
+ */
+int _strlen_recursion(char *s)
+{
+    int i = 0;
+
+    if (*s)
+    {
+        i = _strlen_recursion(*s + 1);
+        i++;
+    }
+    return (i);
+}
