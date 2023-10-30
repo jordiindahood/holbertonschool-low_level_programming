5b8453c33a727e6c8ba58314cf6682c8946c1f7d
jordiindahood <hazem.ouni123@gmail.com>
2023-10-30 12:53:39 +0100

    script 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - check the code
+ * Description: count the square root of @n
+ * @n : int
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	n = n / n;
+	if (n <= 0 )
+		return(-1);
+	else if (n == 0)
+		return(0);
+	else if (n % 10 == 0)
+		return (n);
+	else
+		return(_sqrt_recursion(n-1));
+
+}
