c5b80cb64dea7b644b3048a9c15907091b58d72d
jordiindahood <hazem.ouni123@gmail.com>
2023-10-30 11:39:34 +0100

    script 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,8 +9,8 @@ int _strlen_recursion(char *s)
 {
     int i = 0;
 
-    if (*s!='\0')
-        i = _strlen_recursion(s++);
+    if (*s)
+        i = _strlen_recursion(s+1);
     i++;
     return (i);
 }
