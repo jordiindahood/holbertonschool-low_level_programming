0a9d2b9114ea7d2d8d5f1d0ee5d84bff465954b9
jordiindahood <hazem.ouni123@gmail.com>
2023-10-30 09:47:46 +0100

    script 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - check the code
+ * Description: print a string
+ * @s : pointer to string
+ * Return: Void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+		_puts_recursion(s);
+}
