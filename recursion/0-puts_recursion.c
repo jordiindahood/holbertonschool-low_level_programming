c783b79ebf1b496f5e292b10601daaa55d3bdc17
jordiindahood <hazem.ouni123@gmail.com>
2023-10-30 09:54:48 +0100

    script 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,10 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
 	if (*s != '\0')
-		_puts_recursion(s);
-	else 
-		_putchar('\n');
+	{
+		_putchar(*s);
+		s++;
+		if (*s != '\0')
+			_puts_recursion(s);
+		else
+			_putchar('\n');
+	}
 }
