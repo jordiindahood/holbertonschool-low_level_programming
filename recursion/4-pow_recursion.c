90e49c9661ef7d1b3da47440133dd4ce2290157e
jordiindahood <hazem.ouni123@gmail.com>
2023-10-30 12:25:52 +0100

    script 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,12 +5,12 @@
  * @n : int
  * Return: int
  */
-int pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y > 0)
-		return (x * pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	else if (y == 0)
 		return (1);
 	else
-		return (x / pow_recursion(x, y + 1));
+		return (x / _pow_recursion(x, y + 1));
 }
