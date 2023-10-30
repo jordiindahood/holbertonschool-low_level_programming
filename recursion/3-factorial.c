13c5b5a0313e4b2c5c0c47a57cfbb9875507bb9f
jordiindahood <hazem.ouni123@gmail.com>
2023-10-30 12:09:24 +0100

    script 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -7,8 +7,10 @@
  */
 int factorial(int n)
 {
-    if (n > 0)
-        return (n * factorial(n - 1));
-    else
-        return (1);
+	if (n > 0)
+		return (n * factorial(n - 1));
+	else if (n < 0)
+		return (-1);
+	else
+		return (1);
 }
