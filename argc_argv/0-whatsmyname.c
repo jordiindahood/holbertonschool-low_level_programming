e4b1c072b9b2d7d27ec74780c61f82616460b2c6
jordiindahood <hazem.ouni123@gmail.com>
2023-11-02 11:02:22 +0100

    script 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,11 +2,11 @@
 /**
  * main - check the code
  *@argc : int , number of args including name of the program
- *@argv : array of strings ; 
+ *@argv : array of strings ;
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[argc - 1]);
-    return (0);
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
