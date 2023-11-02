7d7d1d6e295784146a024879dad227da6780782c
jordiindahood <hazem.ouni123@gmail.com>
2023-11-02 11:45:38 +0100

    script 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *@argc : int , number of args including name of the program
+ *@argv : array of strings ;
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+		printf("%d\n", argv[argc] * argv[argc - 1]);
+	else
+		printf("Error\n");
+	return (0);
+}
