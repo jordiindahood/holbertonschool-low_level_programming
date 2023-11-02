13f223c39a47b2d7e82e1db71996010f1cf29ea8
jordiindahood <hazem.ouni123@gmail.com>
2023-11-02 11:40:12 +0100

    script 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *@argc : int , number of args including name of the program
+ *@argv : array of strings ;
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    while (i < argc && *argv[i] != 0)
+    {
+        i++;
+        printf("%s\n", *(argv i - 1));
+    }
+    return (0);
+}
