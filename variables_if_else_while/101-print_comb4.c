f2fe772560bb4e95c2d4aa9ae3b12786c02323a6
jordiindahood <hazem.ouni123@gmail.com>
2023-11-04 15:54:30 +0100

    advanced 101

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ *main - print
+ *Description: print all possible 3 combinations of numbers
+ * Return: zero
+ */
+int main(void)
+{
+    int i, j, k = 0;
+    int space = 0;
+
+    for (k = 48; k < 58; k++)
+    {
+        for (i = 48; i < 58; i++)
+        {
+            for (j = 48; j < 58 && space == 0; j++)
+            {
+                if (k * 100 + i * 10 + j < k * 100 + j * 10 + i)
+                {
+                    putchar(k);
+                    putchar(i);
+                    putchar(j);
+                    space = 1;
+                }
+                if (space == 1 && (k * 100 + i * 10 + j) < 6117)
+                {
+                    putchar(',');
+                    putchar(' ');
+                    space = 0;
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
+}
