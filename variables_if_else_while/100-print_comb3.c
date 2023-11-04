2416ddd6e10133a442fb49c1d0a5c0bbd402cbf2
jordiindahood <hazem.ouni123@gmail.com>
2023-11-04 15:46:55 +0100

    advanced 100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -3,29 +3,29 @@
 #include <stdlib.h>
 /**
  *main - print
- *Void
- *Description: print all possible combinations of
- *single-digit numbers.
+ *Description: print all possible combinations of numbers
  * Return: zero
  */
 int main(void)
 {
 	int i, j = 0;
 	int space = 0;
+
 	for (i = 48; i < 58; i++)
 	{
 		for (j = 48; j < 58 && space == 0; j++)
 		{
-			if (i*10 + j < j*10 + i)
+			if (i * 10 + j < j * 10 + i)
 			{
 				putchar(i);
 				putchar(j);
-				space = 1 ; 
+				space = 1;
 			}
-			if (space == 1 && (i*10 + j) < 617){
-			putchar(',');
-			putchar(' ');
-			space = 0;
+			if (space == 1 && (i * 10 + j) < 617)
+			{
+				putchar(',');
+				putchar(' ');
+				space = 0;
 			}
 		}
 	}
