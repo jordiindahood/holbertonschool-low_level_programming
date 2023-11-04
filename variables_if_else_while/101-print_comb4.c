e034b3ae124523f00c049714c63284b9825986d2
jordiindahood <hazem.ouni123@gmail.com>
2023-11-04 16:02:39 +0100

    advanced 101

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -8,34 +8,34 @@
  */
 int main(void)
 {
-    int i, j, k = 0;
-    int space = 0;
+	int i, j, k = 0;
+	int space = 0;
 
-    for (k = 48; k < 58; k++)
-    {
-        for (i = 48; i < 58; i++)
-        {
-            for (j = 48; j < 58 && space == 0; j++)
-            {
-                if (i * 10 + j < j * 10 + i)
-                {
-                    if (k * 100 + i * 10 + j < i * 100 + k * 10 + j)
-                    {
-                        putchar(k);
-                        putchar(i);
-                        putchar(j);
-                        space = 1;
-                    }
-                }
-                if (space == 1 && (k * 100 + i * 10 + j) < 6117)
-                {
-                    putchar(',');
-                    putchar(' ');
-                    space = 0;
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (k = 48; k < 58; k++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			for (j = 48; j < 58 && space == 0; j++)
+			{
+				if (i * 10 + j < j * 10 + i)
+				{
+					if (k * 100 + i * 10 + j < i * 100 + k * 10 + j)
+					{
+						putchar(k);
+						putchar(i);
+						putchar(j);
+						space = 1;
+					}
+				}
+				if (space == 1 && (k * 100 + i * 10 + j) < 6117)
+				{
+					putchar(',');
+					putchar(' ');
+					space = 0;
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
