177995c653e5d1729f523619a15321f2c62fb1bd
jordiindahood <hazem.ouni123@gmail.com>
2023-11-06 11:28:18 +0100

    script 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -6,20 +6,19 @@
  */
 char *_strdup(char *str)
 {
-	unsigned int i = 0;
-	char *p;
-    int size;
+    unsigned int size, i = 0;
+    char *p;
 
-	size = strlen(str);
+    size = strlen(str);
     if (size < 1)
-		return (NULL);
-	do
-		p = malloc(sizeof(char) * size);
-	while (p == NULL);
-	while (i < size)
-	{
-		p[i] = str[i];
-		i++;
-	}
-	return (p);
+        return (NULL);
+    do
+        p = malloc(sizeof(char) * size);
+    while (p == NULL);
+    while (i < size)
+    {
+        p[i] = str[i];
+        i++;
+    }
+    return (p);
 }
