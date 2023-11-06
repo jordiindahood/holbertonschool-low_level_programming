3615e58cdd991c8da14b99c98c53666c3514d2ee
jordiindahood <hazem.ouni123@gmail.com>
2023-11-06 14:31:58 +0100

    script 1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,26 +7,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    unsigned int size1, size2 = 0;
-    char *p;
-    unsigned int i = 0;
+	unsigned int size1, size2 = 0;
+	char *p;
+	unsigned int i = 0;
 
-    if (s1 == NULL && s2 == NULL)
-        return (NULL);
-    if (s1 != NULL)
-        size1 = strlen(s1);
-    if (s2 != NULL)
-        size2 = strlen(s2);
-    do
-        p = malloc(sizeof(char) * (size1 + size2 + 1));
-    while (p == NULL);
-    while (i < size1 + size2 + 1)
-    {
-        if (i < size1)
-            p[i] = s1[i];
-        else
-            p[i] = s2[i - size1];
-        i++;
-    }
-    return (p);
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	if (s1 != NULL)
+		size1 = strlen(s1);
+	if (s2 != NULL)
+		size2 = strlen(s2);
+	do
+		p = malloc(sizeof(char) * (size1 + size2 + 1));
+	while (p == NULL);
+	while (s2 == NULL && i < size1)
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s1 == NULL && i < size2)
+	{
+		p[i] = s2[i];
+		i++;
+	}
+	while (i < size1 + size2 + 1 && s1 != NULL && s2 != NULL)
+	{
+		if (i < size1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[i - size1];
+		i++;
+	}
+	return (p);
 }
