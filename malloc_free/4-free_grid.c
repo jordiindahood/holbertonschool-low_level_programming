b6b10f949f2c82c4a7d8c164c75c38dc0d8b7212
jordiindahood <hazem.ouni123@gmail.com>
2023-11-08 13:52:06 +0100

    script 4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,14 +2,14 @@
 /**
  * free_grid - free 2d array from memory
  *@grid: array
- *@height: 1st dimension 
+ *@height: 1st dimension
  * Return: ptr.
  */
 void free_grid(int **grid, int height)
 {
-    int i = 0;
+	int i = 0;
 
-	for(i=height-1;i>=0;i--)
-        free(grid[i]);
-    free(grid);
+	for (i = height - 1; i >= 0; i--)
+		free(grid[i]);
+	free(grid);
 }
