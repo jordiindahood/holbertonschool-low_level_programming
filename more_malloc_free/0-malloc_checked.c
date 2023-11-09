c6b5911f7a0d3ffb8091c1a2733671f6ec8750ea
jordiindahood <hazem.ouni123@gmail.com>
2023-11-09 10:01:18 +0100

    script 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * free_grid - free 2d array from memory
- *@grid: array
- *@height: 1st dimension
- * Return: ptr.
+ * malloc_checked - allocate memory
+ *@b: unsigned int , number of bytes
+ * Return: void*
  */
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *p;
+
 	p = (void *)malloc(b);
 	if (p == NULL)
-		return ((void *)98);
+		return (98);
 	return (p);
 }
