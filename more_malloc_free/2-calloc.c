898ed8f4efa476d98d37ce2fd724a21979271b8b
jordiindahood <hazem.ouni123@gmail.com>
2023-11-10 16:02:36 +0100

    script 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,6 +12,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = (void *)calloc(sizeof(nmemb) * size);
+	p = (void *)calloc(nmemb,sizeof(unsigned int));
 	return (p);
 }
