0ae9ebebbc8869f6ea3d58eaed58519fd2a9e4d7
jordiindahood <hazem.ouni123@gmail.com>
2023-11-10 15:54:57 +0100

    script 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_nconcat - concat two strings
+ *@s1: string1
+ *@s2: string2
+ *@n: number of first characters of s2
+ * Return: void*
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    char *p;
+
+    if (nmemb==0 || size==0)
+        return(NULL);
+    char=calloc(sizeof(nmemb)*size);
+}
