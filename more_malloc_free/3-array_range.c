255de78ab648a1d2ff8f5e198f95c4a6477ccef0
jordiindahood <hazem.ouni123@gmail.com>
2023-11-10 16:29:38 +0100

    script 3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * array_range - create an array of integers
+ *@min: int
+ *@max: int
+ * Return: int
+ */
+int *array_range(int min, int max)
+{
+    int *p;
+    int i;
+
+    if (max < min)
+        return (NULL);
+
+    p = (int)malloc(sizeof(int) * (max - min + 1));
+
+    if (p == NULL)
+        return (NULL);
+
+    for (i = 0; i <= max - min + 1; i++)
+        p[i] = min + i;
+
+    return (p);
+}
