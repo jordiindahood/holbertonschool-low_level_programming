c2145cf14839e3fc03a54a2cfe3c7637608bdfae
jordiindahood <hazem.ouni123@gmail.com>
2023-11-13 12:06:02 +0100

    script 4

---
+++ structures_typedef/4-new-dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - create a new dog
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ * Return: dog_t *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t doggy;
+
+		doggy->name = name;
+		doggy->age = age;
+		doggy->owner = owner;
+
+    return(doggy);
+}
