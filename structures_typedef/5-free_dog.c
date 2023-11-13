48e07d934f7345729c76b9327938364a6ae4bf16
jordiindahood <hazem.ouni123@gmail.com>
2023-11-13 14:49:38 +0100

    script 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * free_dog - whoooo let the dogs out? whoof, Whoof Whoof
+ * @d: struct dog *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+        free(d->age);
+        free(d->owner);
+        free(d);
+	}
+}
