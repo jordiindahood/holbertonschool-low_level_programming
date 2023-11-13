2f26710c3521c9c05b5a458ae69a425a02c1fae9
jordiindahood <hazem.ouni123@gmail.com>
2023-11-13 14:41:39 +0100

    script 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,16 +14,47 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
-
 	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
+	d->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d);
+		free(d->name);
+		return (NULL);
+	}
 
-	d->name = name;
+	_strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
-
+	_strcpy(d->owner, owner);
 	return (d);
+}
+/**
+ * _strcpy - check the code
+ * Description: copy an array to another
+ * @dest : pointer to destination array
+ * @src : pointer to be copied
+ * Return: char*
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
