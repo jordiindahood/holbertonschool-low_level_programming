f68e4eade43df35a3abc76221a452d793ac2d5df
jordiindahood <hazem.ouni123@gmail.com>
2023-11-13 12:32:28 +0100

    script 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,14 +12,18 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+
 	d = malloc(sizeof(dog_t));
 
 	if (d == NULL)
+	{
+		free(d);
 		return (NULL);
-
-	d->name = name;
+	}
+	
+	d->name = strcpy(d->name,name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = strcpy(d->owner,owner;
 
 	return (d);
 }
