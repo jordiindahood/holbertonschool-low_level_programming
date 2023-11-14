af8043b7a23363fd76ee30c25a1c54f5f5065340
jordiindahood <hazem.ouni123@gmail.com>
2023-11-14 12:02:37 +0100

    script 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "function_pointers.h"
+/**
+ * int_index - execute a func
+ * @array: int*
+ * @size: int
+ * @cmp: pointer to function
+ * Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0 && array != NULL && cmp != NULL)
+		for (i = 0; i < size; i++)
+			(*cmp)(array[i]);
+}
