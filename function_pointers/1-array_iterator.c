5f710389e5a42599d841cec67b8b03f0ea3b7eea
jordiindahood <hazem.ouni123@gmail.com>
2023-11-14 10:36:26 +0100

    script 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,9 +11,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    if (size > 0 && array != NULL)
-        for (i = 0; i < size; i++)
-            (*action)(array[i]);
+	if (size > 0 && array != NULL && action != NULL)
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
 }
