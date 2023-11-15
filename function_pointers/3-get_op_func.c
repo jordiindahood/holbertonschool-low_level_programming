d3e9c28adc13d6a2cb5dddaa8ca3186626f7e88f
jordiindahood <hazem.ouni123@gmail.com>
2023-11-15 21:24:24 +0100

    script 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,9 +19,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (i < 6 && i != 5 )
 	{
-		if (strcmp(ops[i].op, s) == 0 && i != 5)
+		if (strcmp(ops[i].op, s) == 0 )
 			return (ops[i].f);
 		i++;
 	}
