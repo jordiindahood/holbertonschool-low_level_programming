94c95ad1eeb42575fba18f08333c83be11ac0cbe
jordiindahood <hazem.ouni123@gmail.com>
2023-11-17 15:29:56 +0100

    script 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
         va_start(p, n);
 
         for (i = 0; i < n; i++)
-            if (va_arg(p, int) != NULL)
+            if (va_arg(p, int))
                 printf("%d", va_arg(p, int));
 
         printf("\n");
