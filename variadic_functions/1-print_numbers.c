390c4e2e33545be682c69fd0d6b524c199258df9
jordiindahood <hazem.ouni123@gmail.com>
2023-11-17 15:26:46 +0100

    script 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_numbers - print all the arguments
+ * @separator: const char *
+ * @n : const unsigned int
+ * Return: int
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    unsigned int i;
+    va_list p;
+
+    if (separator != NULL)
+    {
+        va_start(p, n);
+
+        for (i = 0; i < n; i++)
+            printf("%d", va_arg(p, int));
+
+        printf("\n");
+        va_end(p);
+    }
+}
