989bfcf0c41f1477b4a39591c7e00e70229a7fae
jordiindahood <hazem.ouni123@gmail.com>
2023-11-28 12:27:03 +0100

    script 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -6,14 +6,17 @@
  */
 size_t list_len(const list_t *h)
 {
-    list_t *p;
-    size_t i = 0;
+	list_t *p;
+	size_t i = 0;
 
-    if (h)
-    {
-        p = (list_t *)h;
-        while (p)
-            i++;
-    }
-    return (i);
+	if (h)
+	{
+		p = (list_t *)h;
+		while (p)
+		{
+			p = p->next;
+			i++;
+		}
+	}
+	return (i);
 }
