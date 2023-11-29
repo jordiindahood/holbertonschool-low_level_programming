15350ad4223ba69539bd90e22aee21868e16724d
jordiindahood <hazem.ouni123@gmail.com>
2023-11-29 17:32:57 +0100

    100

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -5,5 +5,6 @@
  */
 void im_fast_asf_boiiiiii(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf(" bore my house upon my back!\n");
 }
