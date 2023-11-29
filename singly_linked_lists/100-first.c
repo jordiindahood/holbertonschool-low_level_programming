ddd4ccab04428a095a4be34dd56a3686fb08d116
jordiindahood <hazem.ouni123@gmail.com>
2023-11-29 17:32:07 +0100

    100

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * im_fast_asf_boiiiiii - execute before the main func
+ * Return: void
+ */
+void im_fast_asf_boiiiiii(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
