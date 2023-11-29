bd140365188c96a5395f83727a46968b71a6e768
jordiindahood <hazem.ouni123@gmail.com>
2023-11-29 13:05:36 +0100

    script 4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 /**
  * free_list - free list
- * @head: list **
+ * @head: list *
  * Return: list_t
  */
 void free_list(list_t *head)
 {
-	list_t *p;
-
 	while (head != NULL)
 	{
+		list_t *p;
+
 		p = head;
 		head = head->next;
 		free(p);
