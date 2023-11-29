bb43d0ef7e9c87affe3873fd8e05b21c03538b07
jordiindahood <hazem.ouni123@gmail.com>
2023-11-29 13:15:05 +0100

    script 4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -6,14 +6,13 @@
  */
 void free_list(list_t *head)
 {
-   list_t* tmp;
+	list_t *tmp;
 
-   while (head != NULL)
-    {
-       tmp = head;
-       head = head->next;
-	   free(tmp->str);
-       free(tmp);
-    }
-
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
 }
