bfa80a8a6b7e72a660477a17f00bda7b29c3df2d
jordiindahood <hazem.ouni123@gmail.com>
2023-11-30 18:08:59 +0100

    script 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,64 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete a node at a specific node
+ * @h: dlistint_t **
+ * @idx: unsigned int
+ * @n: int
+ * Return: dlistint_t
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *target, *pre, *aft;
+	unsigned int i;
+
+	if (index > dlistint_len(*head) || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		target = *head;
+		*head = target->next;
+		free(target);
+		return (1);
+	}
+
+	if (index == (unsigned int)dlistint_len(*head))
+		return (1);
+	for (i = 0; target->next; i++)
+	{
+		if (i == index)
+			break;
+		target = target->next;
+	}
+
+	pre = target->prev;
+	aft = target->next;
+	free(target);
+
+	if (aft)
+		aft->prev = pre;
+	pre->next = aft;
+
+	return (1);
+}
+/**
+ * dlistint_len - return the number of elements in a the list
+ * @h: const dlistint_t *
+ * Return: size_t number of elements.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *p;
+	size_t i = 0;
+
+	if (h)
+	{
+		p = (dlistint_t *)h;
+		while (p)
+		{
+			p = p->next;
+			i++;
+		}
+	}
+	return (i);
+}
