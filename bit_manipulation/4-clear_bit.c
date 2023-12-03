ec0e384061b651796637774958197eaf4f1a14c9
jordiindahood <hazem.ouni123@gmail.com>
2023-12-03 19:12:49 +0100

    script 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * clear_bit - set a value of a bit at a given index to 0
+ * @n: unsigned long int *
+ * @index: unsigned int
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (!!(*n |= 0L << index));
+}
