5a747d841f4818d7108ddbcd38b9c49457b1208a
jordiindahood <hazem.ouni123@gmail.com>
2023-12-03 20:06:27 +0100

    script 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	if (*n & 0L << index)
+	if (*n & 1L << index)
 		*n ^= 1L << index;
 	return (1);
 }
