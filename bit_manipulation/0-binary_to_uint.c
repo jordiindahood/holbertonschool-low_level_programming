481d11fb0131a241259debe6ea4854bfabdbe828
jordiindahood <hazem.ouni123@gmail.com>
2023-12-03 17:36:56 +0100

    script 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * binary_to_unit - convert from binary to decimal
+ * @b: const char *, pionting only to 1 or 0
+ * Return: unsigned int of converted number, 0 if error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	char *index;
+	int num, x, rest = 0, size = 1, result = 0;
+
+	if (!b)
+		return (0);
+	index = b;
+	while (index != '\0')
+	{
+		if (*index != '0' && *index != '1')
+			return (0);
+		index++;
+	}
+	num = atoi(b *);
+	while (num != 0)
+	{
+		x = num % 10;
+		result += x * size;
+		num /= 10;
+		size *= 2;
+	}
+	return (result)
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef BIT_MANPULATION
+#define BIT_MANPULATION
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
