350718254c537ebff466300c46020ea82205c1dd
jordiindahood <hazem.ouni123@gmail.com>
2023-12-03 19:09:03 +0100

    script 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_bit - return a value of a bit at a given index
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (!!(*n |= 1L << index));
+}
