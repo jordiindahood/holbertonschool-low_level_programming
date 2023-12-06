906a4feefd8d85f83ede717adb936ebef7e7f0bf
jordiindahood <hazem.ouni123@gmail.com>
2023-12-06 16:35:51 +0100

    final push

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,12 +9,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
 	ssize_t bytes;
-	char buffer[1024];
+	char buffer[1024 * 69];
 
 	if (!filename || !letters)
 		return (0);
 
-	file = open(filename, O_RDONLY);
+	file = open(filename, "r");
 	if (file == -1)
 		return (0);
 
