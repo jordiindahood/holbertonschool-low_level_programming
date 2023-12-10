e415b6f2b9a4374fc92a760b8a0aa2deefa45fd4
jordiindahood <hazem.ouni123@gmail.com>
2023-12-10 22:59:59 +0100

    script 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(node->value);
 			node->value = strdup(value);
-			if (!node->value)
+			if (!node1->value)
 				return (0);
 			return (1);
 		}
