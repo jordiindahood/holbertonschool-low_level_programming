353313003bad183b0b471e175d84c26e7e7e57ca
jordiindahood <hazem.ouni123@gmail.com>
2023-12-10 22:59:34 +0100

    script 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 			return (1);
 		}
-		node1 = node->next;
+		node1 = node1->next;
 	}
 	node2 = malloc(sizeof(hash_node_t));
 	if (!node2)
