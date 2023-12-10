09edfccd67cc4a27d02f973087e1a59e0b540a25
jordiindahood <hazem.ouni123@gmail.com>
2023-12-10 21:51:58 +0100

    script 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,40 +1,33 @@
 #include "main.h"
 /**
- * create_file - create a file filled with a text
- * @filename: const char *
- * @text_content: char*
- * Return: number of bytes or 0 if error
+ * cp_textfile - check the code
+ * @file_from: char
+ * @file_to: size_t
+ * Return: Always 0.
  */
-ssize_t cp(const char *src, const char *dest)
+ssize_t cp_textfile(const char *file_from, const char *file_to)
 {
 	int fd, fd1;
 	ssize_t b;
 	char buf[1024];
 
-	fd = open(src, O_RDONLY);
-
+	fd = open(file_from, O_RDONLY);
 	if (fd == -1)
-		dprintf(1, FILE_FROM_NOT_EXIST, src), exit(98);
-
-	fd1 = open(src, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		dprintf(1, FILE_FROM_NOT_EXIST, file_from), exit(98);
+	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd1 == -1)
-		dprintf(2, FILE_TO_ERROR, dest), exit(99);
-
+		dprintf(2, FILE_TO_ERROR, file_to), exit(99);
 	while ((b = read(fd, buf, 1024)) > 0)
 		if (write(fd1, buf, b) != b)
-			dprintf(2, FILE_TO_ERROR, dest), exit(99);
-
+			dprintf(2, FILE_TO_ERROR, file_to), exit(99);
 	if (b == -1)
-		dprintf(2, FILE_FROM_NOT_EXIST, src), exit(98);
-
+		dprintf(2, FILE_FROM_NOT_EXIST, file_from), exit(98);
 	fd = close(fd);
 	fd1 = close(fd1);
-
 	if (fd == -1)
 		dprintf(2, DESCRIPTOR_NOT_CLOSE, fd), exit(100);
 	if (fd1 == -1)
 		dprintf(2, DESCRIPTOR_NOT_CLOSE, fd1), exit(100);
-
 	return (0);
 }
 /**
