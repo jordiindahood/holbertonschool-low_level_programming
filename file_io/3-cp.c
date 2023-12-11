376b933b0bd1cce8ae31221d226bc3510124df23
jordiindahood <hazem.ouni123@gmail.com>
2023-12-11 12:52:38 +0100

    script 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,16 +14,22 @@ ssize_t cp_textfile(const char *file_from, const char *file_to)
 	fd = open(file_from, O_RDONLY);
 	if (fd == -1)
 		dprintf(1, FILE_FROM_NOT_EXIST, file_from), exit(98);
+
 	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
 	if (fd1 == -1)
 		dprintf(2, FILE_TO_ERROR, file_to), exit(99);
+
 	while ((b = read(fd, buf, 1024)) > 0)
 		if (write(fd1, buf, b) != b)
 			dprintf(2, FILE_TO_ERROR, file_to), exit(99);
+
 	if (b == -1)
 		dprintf(2, FILE_FROM_NOT_EXIST, file_from), exit(98);
+
 	fd = close(fd);
 	fd1 = close(fd1);
+
 	if (fd == -1)
 		dprintf(2, DESCRIPTOR_NOT_CLOSE, fd), exit(100);
 	if (fd1 == -1)
